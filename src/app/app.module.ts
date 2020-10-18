258ce5d761e2366a283eb8ead18e994764b28e6c
ykto1234 <to.46101753@gmail.com>
2020-10-18 23:10:48 +0900

    23.ルーティングの設定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,33 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent }  from './app.component';
 import { MemberDetailComponent } from './member-detail.component';
+import { MembersComponent } from './members.component';
+import { MemberService } from './member.service';
 
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'members',
+        component: MembersComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    MemberDetailComponent
+    MembersComponent,
+    MemberDetailComponent,
   ],
   bootstrap:    [
     AppComponent
-  ]
+  ],
+  providers: [ MemberService ]
 })
 export class AppModule { }
