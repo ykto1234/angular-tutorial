b81699af6481293fc0b20eddf23a65db42fe7f27
ykto1234 <to.46101753@gmail.com>
2020-10-19 22:19:35 +0900

    27.HTTPクライアントでデータを取得する

--- src/app/member-detail.component.ts
+++ src/app/member-detail.component.ts
@@ -31,4 +31,9 @@ export class MemberDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+
+  save(): void {
+    this.memberService.update(this.member)
+      .then(() => this.goBack());
+  }
 }
--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -1,16 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Member } from './member';
 import { MEMBERS } from './mock-members';
 
 @Injectable()
 export class MemberService {
+
+  membersUrl = 'api/members';
+
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+
+  constructor(private http: Http) { }
+
   getMembers(): Promise<Member[]> {
-    return Promise.resolve(MEMBERS);
+    return this.http.get(this.membersUrl)
+      .toPromise()
+      .then(response => response.json().data as Member[])
+      .catch(this.handleError);
   }
 
   getMember(id: number): Promise<Member> {
-    return this.getMembers()
-      .then(members => members.find(member => member.id === id));
+    const url = `${this.membersUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Member)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('エラー', error);
+    return Promise.reject(error.message || error);
+  }
+
+  update(member: Member): Promise<Member> {
+    const url = `${this.membersUrl}/${member.id}`;
+    return this.http.put(url, JSON.stringify(member), { headers: this.headers })
+      .toPromise()
+      .then(() => member)
+      .catch(this.handleError);
   }
 }
